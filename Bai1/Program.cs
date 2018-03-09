67c2ec45bfb20a5e9a2906721bf0c0992ffbb96f
freeze159 <tieuduy97@gmail.com>
2018-03-09 19:35:00 +0700

    first commit

---
+++ Bai1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress[] ips;
+            string hostname = Dns.GetHostName();
+            ips = Dns.GetHostAddresses(hostname);
+            Console.WriteLine("GetHostAddresses ({0}) return ", hostname);
+            foreach (IPAddress ip in ips)
+            {
+                Console.WriteLine("     {0}", ip);
+            }
+            
+            
+            
+
+        }
+    }
+}
---
+++ Bai2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Bai2
+{
+    class Program
+    {
+        static void GetIp(string hostname)
+        {
+            
+            IPAddress[] listIP = Dns.GetHostAddresses(hostname);
+            foreach (IPAddress ip in listIP)
+            {
+                Console.WriteLine("         {0} ", ip);
+            }
+        }
+        static string GetHostName(string ip)
+        {
+            IPHostEntry entry = Dns.GetHostEntry(ip);
+            if (entry!=null)
+            {
+                return entry.HostName;
+            }
+            return "Khong tim thay";
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ten mien can phan giai: ");
+            string hostname = Console.ReadLine();
+            GetIp(hostname);
+            string ip = "172.16.0.213";
+            Console.WriteLine(GetHostName(ip));
+        }
+    }
+}
